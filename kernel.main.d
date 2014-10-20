64387f021598ef1366416acb876063d193eb4885
Nassim 'Nass' Eddequiouaq <nass@lse.epita.fr>
2014-10-20 18:03:49 +0200

    small malloc, realloc, string’s concatenation

--- kernel.main.d
+++ kernel.main.d
@@ -4,53 +4,92 @@ import console;
 import utils;
 import gdt;
 
+extern(C) extern __gshared void *_data_end;
+static __gshared void *brk = &_data_end;
+
+extern(D) auto malloc(T)(size_t size) {
+    void *res = brk;
+    brk = (cast(char *)brk + size);
+
+    return cast(T)res;
+}
+
+extern(D) void free(void *loltarace) {}
+
+extern(D) auto realloc(T)(void *ptr, size_t size) {
+    if (!ptr)
+        return malloc!(T)res;
+    if (!size && ptr)
+        return null;
+
+    malloc!(T)(res);
+    memcpy!(T)(res, ptr, size);
+    return res;
+}
+
+extern(D) auto strlen(T)(T s) {
+    auto i = 0;
+
+    for (; *s; ++s)
+        ++i;
+
+    return i;
+}
+
+extern(C) void memcpy(T)(T dest, T src, size_t size) {
+    ubyte *tdest = cast(ubyte *)dest;
+    ubyte *tsrc = cast(ubyte *)src;
+    for (size_t i = 0; i < size; ++i)
+        tdest[i] = tsrc[i];
+}
+
+extern(C) void memset(T)(T dest, ubyte c, size_t size) {
+    ubyte *tdest = cast(ubyte *)dest;
+    for (size_t i = 0; i < size; ++i)
+        tdest[i] = c;
+}
+
+extern(C) void _D15TypeInfo_Struct6__vtblZ() {}
+extern(C) void _D11TypeInfo_Aa6__initZ() {}
+extern(C) void _D12TypeInfo_Aya6__initZ() {}
+
+// FIXME
+extern(C) void *_adDupT() {return null;}
+
+extern(D) char[] _d_arraycatT(void *ti, char s1[],char s2[]) {
+//    uint size1 = strlen!(char *)(cast(char *)s1);
+//    uint size2 = strlen!(char *)(cast(char *)s2);
+
+    char *ptr = malloc!(char *)(s1.length + s2.length + 1);
+    if (ptr) {
+        memcpy!(char *)(ptr, s1.ptr, s1.length);
+        memcpy!(char *)(ptr + s1.length, s2.ptr, s2.length);
+        ptr[s1.length + s2.length + 1] = '\0';
+        return ptr[0 .. s1.length + s2.length];
+    }
+
+
+    void *r = malloc!(void *)(size_t.sizeof * 2);
+
+    (cast(size_t *)r)[0] = s1.length + s2.length;
+    (cast(size_t *)r)[1] = cast(size_t)ptr;
+
+    return cast(char[])*cast(char[] *)&r;
+}
+
+extern(C) void *_Dmodule_ref() { return null;}
 
 extern(C) void main(uint magic, uint addr) {
     clear();
     write("test\n", 4);
-/*
-    int ypos = 0;
-    int xpos = 0;
-    const uint COLUMNS = 80;
-    const uint LINES = 25;
-
-    ubyte* vidmem = cast(ubyte*)0xFFFF8000000B8000; //Video memory address
-    mixin("xpos = 20;");
-
-    for (int i = 0; i < COLUMNS * LINES * 2; i++) {
-            *(vidmem + i) = 0x0;
-    }
-
-    *(vidmem + (xpos + ypos * COLUMNS) * 2 + 1) = 0x07;
-    *(vidmem + (xpos++ + ypos * COLUMNS) * 2) = 'T' & 0xFF;
-    *(vidmem + (xpos + ypos * COLUMNS) * 2 + 1) = 0x07;
-    *(vidmem + (xpos++ + ypos * COLUMNS) * 2) = 'E' & 0xFF;
-    *(vidmem + (xpos + ypos * COLUMNS) * 2 + 1) = 0x07;
-    *(vidmem + (xpos++ + ypos * COLUMNS) * 2) = 'S' & 0xFF;
-    *(vidmem + (xpos + ypos * COLUMNS) * 2 + 1) = 0x07;
-    *(vidmem + (xpos++ + ypos * COLUMNS) * 2) = 'T' & 0xFF;
-    *(vidmem + (xpos + ypos * COLUMNS) * 2 + 1) = 0x07;
-    *(vidmem + (xpos++ + ypos * COLUMNS) * 2) = ' ' & 0xFF;
-    *(vidmem + (xpos + ypos * COLUMNS) * 2 + 1) = 0x07;
-    *(vidmem + (xpos++ + ypos * COLUMNS) * 2) = ' ' & 0xFF;
-    *(vidmem + (xpos + ypos * COLUMNS) * 2 + 1) = 0x07;
-    *(vidmem + (xpos++ + ypos * COLUMNS) * 2) = '1' & 0xFF;
-    *(vidmem + (xpos + ypos * COLUMNS) * 2 + 1) = 0x07;
-    *(vidmem + (xpos++ + ypos * COLUMNS) * 2) = '2' & 0xFF;
-    *(vidmem + (xpos + ypos * COLUMNS) * 2 + 1) = 0x07;
-    *(vidmem + (xpos++ + ypos * COLUMNS) * 2) = '3' & 0xFF;
-    *(vidmem + (xpos + ypos * COLUMNS) * 2 + 1) = 0x07;
-    *(vidmem + (xpos++ + ypos * COLUMNS) * 2) = ' ' & 0xFF;
-    *(vidmem + (xpos + ypos * COLUMNS) * 2 + 1) = 0x07;
-    *(vidmem + (xpos++ + ypos * COLUMNS) * 2) = '!' & 0xFF;
-
-    */
     init_gdt();
-// `_D11TypeInfo_Aa6__initZ' and `_d_arraycatT'
+
+    string lol1 = "1lolok1";
+    string lol2 = "2lolok2";
+    string lol3 = lol1 ~ lol2;
+    write(cast(char *)lol3.ptr, 1);
+    write_de
 
     while (1) {
     }
 }
-
-extern(C) void _D15TypeInfo_Struct6__vtblZ() {}
-extern(C) void *_Dmodule_ref() { return null;}
